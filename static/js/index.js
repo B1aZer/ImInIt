4ff3d2a529cd36ae70b8f223e87d319c50c7d5b4
B1aZer <blaze.imba@gmail.com>
2012-03-12 00:34:33 +0400

    css colors

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 
 //$('.formin').hide();
-//iminit.map_init();
+iminit.map_init();
 //iminit.getMarkers();
-
-
-
+$(".btn:contains('View')").click(function() {
+    $(".wrap").animate({
+        marginLeft: '-=20'
+    },1000);
+});
 });
