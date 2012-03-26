c413d236498f5128ba4a5dbb234e7f089a01c65f
B1aZer <blaze.imba@gmail.com>
2012-03-27 00:46:00 +0400

    deploying on heroku 4

--- application/config.py
+++ application/config.py
@@ -1,7 +1,7 @@
 # configuration
 DATABASE_URL = 'postgresql://postgres:qwe123asd123@localhost/postgres'
 #SQLALCHEMY_DATABASE_URI= 'sqlite:///sql.db'
-SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:qwe123asd123@localhost/postgres'
+SQLALCHEMY_DATABASE_URI= DATABASE_URL
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
