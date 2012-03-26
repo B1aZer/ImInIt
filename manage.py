5e03413f6fe47621be5436c91b99c6bfbe781ef9
B1aZer <blaze.imba@gmail.com>
2012-03-27 00:58:11 +0400

    deploying on heroku 6

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import os
 from application import app, create_db, drop_db
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
     #create_db()
     #drop_db()
 
